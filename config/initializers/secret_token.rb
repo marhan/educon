fe790398e710bd942e251230548492390ccac866
Markus Hanses <markus@markushanses.de>
2011-09-25 12:32:23 +0200

    Project created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Educon::Application.config.secret_token = 'c470659e930245e8b42793e087778f59a6a3ec713c6e49e0849da8b42a2e0be3927474d3f4e83db6dd3db9ac0fd6220cd7cad012ae366330b6ab2f5bda207636'
