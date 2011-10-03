3138688febf907169a6c7e9fb8bb7e50c3664c75
Markus Hanses <markus@markushanses.de>
2011-10-03 16:33:49 +0200

    Fix for Travis-CI builds.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,15 @@
 require 'rubygems'
-require 'spork'
 
-Spork.prefork do
+# Configuration for Travis-CI
+if ENV['TRAVIS']
+
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
   ActiveSupport::Dependencies.clear
   ActiveRecord::Base.instantiate_observers
-end
 
-Spork.each_run do
   FactoryGirl.factories.clear
   Dir[Rails.root.join("spec/factories/**/*.rb")].each { |f| load f }
   Educon::Application.reload_routes!
@@ -25,6 +24,40 @@ Spork.each_run do
       init_haml_helpers
     end
   end
+
+else
+  
+  require 'spork'
+
+  Spork.prefork do
+    ENV["RAILS_ENV"] ||= 'test'
+    require File.expand_path("../../config/environment", __FILE__)
+    require 'rspec/rails'
+    Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+    ActiveSupport::Dependencies.clear
+    ActiveRecord::Base.instantiate_observers
+  end
+
+  Spork.each_run do
+    FactoryGirl.factories.clear
+    Dir[Rails.root.join("spec/factories/**/*.rb")].each { |f| load f }
+    Educon::Application.reload_routes!
+
+    RSpec.configure do |config|
+      config.mock_with :rspec
+      config.fixture_path               = "#{::Rails.root}/s pec/fixtures "
+      config.use_transactional_fixtures = true
+      config.include Haml::Helpers
+      config.include ActionView::Helpers
+      config.before(:each) do
+        init_haml_helpers
+      end
+    end
+  end
+  
 end
+
+
+
 
 
