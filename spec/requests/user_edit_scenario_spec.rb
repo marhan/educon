c53ae158ecc319aa193215bd2209a65f0e61b300
Markus Hanses <markus@markushanses.de>
2011-12-04 20:49:21 +0100

    Change test from method to yaml based config.

--- spec/requests/user_edit_scenario_spec.rb
+++ spec/requests/user_edit_scenario_spec.rb
@@ -10,11 +10,11 @@ describe "User" do
     end
 
     it "should be able to edit his account" do
-      visit path_to_users_sign_out
-      visit path_to_users_sign_in_page
+      visit @config.path.to.users.sign.out
+      visit @config.path.to.users.sign.in
       sign_in_user @user
       user_should_be_signed_in
-      click_link @config.account
+      click_link @config.link.edit.account
 
     end
   end
