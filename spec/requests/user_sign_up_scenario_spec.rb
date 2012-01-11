bb9f15b6d32989883a393d97b4c9ab45efe8aea5
Markus Hanses <markus@markushanses.de>
2012-01-11 21:31:33 +0100

    Sign up request scene improved.

--- spec/requests/user_sign_up_scenario_spec.rb
+++ spec/requests/user_sign_up_scenario_spec.rb
@@ -14,19 +14,62 @@ describe "Given user is not signed up" do
       visit '/users/sign_up'
     end
 
-    describe "when he enters valid data," do
+    describe "when the page is rendered" do
 
-      before(:each) do
-        fill_in('Firstname', :with => 'Testy')
-        fill_in('Lastname', :with => 'McUserton')
-        fill_in('Email', :with => 'user@test.com')
-        fill_in('Password', :with => 'please')
-        fill_in('Password confirmation', :with => 'please')
-        click_button('button_signup')
+      it "than he see the expected content" do
+        page.should have_content("Willkommen")
+        page.should have_content("hier kannst du dein Profil erstellen")
+
+        page.should have_content("Registrieren")
+
+        page.should have_content('Vorname')
+        page.should have_content('Nachname')
+        page.should have_content('Email')
+        page.should have_content('Passwort')
+        page.should have_content("Passwort Wiederholung")
+
+        find_button('Anlegen')
+        find_button('Zurücksetzen')
+
+        page.should have_content("Verwandte Links")
+        find_link('Passwort vergessen?')
       end
 
-      it "than he signed up successfully" do
-        page.should have_content I18n.t('devise.registrations.signed_up')
+    end
+
+    describe "when he enters valid data'," do
+
+      before(:each) do
+        fill_in('field_firstname', :with => 'Testy')
+        fill_in('field_lastname', :with => 'McUserton')
+        fill_in('field_email', :with => 'user@test.com')
+        fill_in('field_password', :with => 'please')
+        fill_in('field_password_confirmation', :with => 'please')
+      end
+
+      describe "and click button 'Zurücksetzen'" do
+
+        before(:each) do
+          click_button('button_reset')
+        end
+
+        it "than all entered data is removed" do
+          #TODO: not working!
+          #find_field('field_firstname').value.should == ""
+        end
+
+      end
+
+      describe "and click button 'Anlegen" do
+
+        before do
+          click_button('button_signup')
+        end
+
+        it "than he signed up successfully" do
+          page.should have_content I18n.t('devise.registrations.signed_up')
+        end
+
       end
 
     end
@@ -34,11 +77,11 @@ describe "Given user is not signed up" do
     describe "when he signs up with invalid email" do
 
       before(:each) do
-        fill_in('Firstname', :with => 'Testy')
-        fill_in('Lastname', :with => 'McUserton')
-        fill_in('Email', :with => 'invalidemail')
-        fill_in('Password', :with => 'please')
-        fill_in('Password confirmation', :with => 'please')
+        fill_in('field_firstname', :with => 'Testy')
+        fill_in('field_lastname', :with => 'McUserton')
+        fill_in('field_email', :with => 'invalidemail')
+        fill_in('field_password', :with => 'please')
+        fill_in('field_password_confirmation', :with => 'please')
         click_button('button_signup')
       end
 
@@ -51,11 +94,11 @@ describe "Given user is not signed up" do
     describe "when he signs up without a password" do
 
       before(:each) do
-        fill_in('Firstname', :with => 'Testy')
-        fill_in('Lastname', :with => 'McUserton')
-        fill_in('Email', :with => 'invalidemail')
-        fill_in('Password', :with => '')
-        fill_in('Password confirmation', :with => 'please')
+        fill_in('field_firstname', :with => 'Testy')
+        fill_in('field_lastname', :with => 'McUserton')
+        fill_in('field_email', :with => 'invalidemail')
+        fill_in('field_password', :with => '')
+        fill_in('field_password_confirmation', :with => 'please')
         click_button('button_signup')
       end
 
@@ -68,11 +111,11 @@ describe "Given user is not signed up" do
     describe "when he signs up without password confirmation" do
 
       before(:each) do
-        fill_in('Firstname', :with => 'Testy')
-        fill_in('Lastname', :with => 'McUserton')
-        fill_in('Email', :with => 'invalidemail')
-        fill_in('Password', :with => 'please')
-        fill_in('Password confirmation', :with => '')
+        fill_in('field_firstname', :with => 'Testy')
+        fill_in('field_lastname', :with => 'McUserton')
+        fill_in('field_email', :with => 'invalidemail')
+        fill_in('field_password', :with => 'please')
+        fill_in('field_password_confirmation', :with => '')
         click_button('button_signup')
       end
 
